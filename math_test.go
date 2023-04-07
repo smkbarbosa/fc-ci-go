1417c22a5697ece9a2515351b49ec5f7c5aeb478
Samuel Barbosa <contato@smkbarbosa.eti.br>
2023-04-06 22:34:21 -0300

    fix: ajusta funcao soma

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := soma(15, 15)
+	total := Soma(15, 15)
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
